00e0d83d97ac50659edbdc5956517bdb5e516c8e
Phil <118385120+SpunMuffins@users.noreply.github.com>
2023-07-18 14:41:17 -0400

    changes to personalize
    changes personalizing

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -6,8 +6,8 @@ const matter = require('gray-matter')
 async function generate() {
   const feed = new RSS({
     title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    site_url: 'https://www.philgreene.net',
+    feed_url: 'https://wwww.philgreene.net/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
